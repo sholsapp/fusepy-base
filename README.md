ac5165d89432e5e02b4308b9536e61a79f07d671
Stephen Holsapple <sholsapp@gmail.com>
2014-06-20 10:19:37 -0700

    Cleanup + Documentation

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ fusepy-base
 
 A base fuse plugin written in Python adapted from
 http://www.stavros.io/posts/python-fuse-filesystem/.
+
+installation
+============
+
+On some systems, you may need to change the permissions of fusermount by
+running `chmod 4755 /usr/bin/fusermount`. Refer to the fuse4x FAQ at
+https://github.com/fuse4x/fuse/blob/master/FAQ#L244 for more information.
--- fusepybase/passthrough.py
+++ fusepybase/passthrough.py
@@ -8,42 +8,36 @@ from fuse import FuseOSError, Operations
 
 
 class Passthrough(Operations):
+  """A simple passthrough interface.
+
+  This class passes all file system operations through to the operating
+  system.
+
+  """
   def __init__(self, root):
     self.root = root
 
-  # Helpers
-  # =======
-
-  def _full_path(self, partial):
-    if partial.startswith("/"):
-      partial = partial[1:]
-    path = os.path.join(self.root, partial)
-    return path
-
-  # Filesystem methods
-  # ==================
-
   def access(self, path, mode):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     if not os.access(full_path, mode):
       raise FuseOSError(errno.EACCES)
 
   def chmod(self, path, mode):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     return os.chmod(full_path, mode)
 
   def chown(self, path, uid, gid):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     return os.chown(full_path, uid, gid)
 
   def getattr(self, path, fh=None):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     st = os.lstat(full_path)
     return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
   def readdir(self, path, fh):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
 
     dirents = ['.', '..']
     if os.path.isdir(full_path):
@@ -52,7 +46,7 @@ class Passthrough(Operations):
       yield r
 
   def readlink(self, path):
-    pathname = os.readlink(self._full_path(path))
+    pathname = os.readlink(os.path.abspath(path))
     if pathname.startswith("/"):
       # Path name is absolute, sanitize it.
       return os.path.relpath(pathname, self.root)
@@ -60,24 +54,24 @@ class Passthrough(Operations):
       return pathname
 
   def mknod(self, path, mode, dev):
-    return os.mknod(self._full_path(path), mode, dev)
+    return os.mknod(os.path.abspath(path), mode, dev)
 
   def rmdir(self, path):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     return os.rmdir(full_path)
 
   def mkdir(self, path, mode):
-    return os.mkdir(self._full_path(path), mode)
+    return os.mkdir(os.path.abspath(path), mode)
 
   def statfs(self, path):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     stv = os.statvfs(full_path)
     return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
       'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
       'f_frsize', 'f_namemax'))
 
   def unlink(self, path):
-    return os.unlink(self._full_path(path))
+    return os.unlink(os.path.abspath(path))
 
   def symlink(self, target, name):
     return os.symlink(self._full_path(target), self._full_path(name))
@@ -89,17 +83,14 @@ class Passthrough(Operations):
     return os.link(self._full_path(target), self._full_path(name))
 
   def utimens(self, path, times=None):
-    return os.utime(self._full_path(path), times)
-
-  # File methods
-  # ============
+    return os.utime(os.path.abspath(path), times)
 
   def open(self, path, flags):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     return os.open(full_path, flags)
 
   def create(self, path, mode, fi=None):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
 
   def read(self, path, length, offset, fh):
@@ -111,7 +102,7 @@ class Passthrough(Operations):
     return os.write(fh, buf)
 
   def truncate(self, path, length, fh=None):
-    full_path = self._full_path(path)
+    full_path = os.path.abspath(path)
     with open(full_path, 'r+') as f:
       f.truncate(length)
 
