e4b387948638cf8f66b528a6396cb474ba883fe0
Stephen Holsapple <sholsapp@gmail.com>
2017-03-10 10:52:32 -0800

    Reindent to 4 spaces.

--- fusepybase/passthrough.py
+++ fusepybase/passthrough.py
@@ -14,156 +14,165 @@ log = logging.getLogger(__name__)
 
 
 def logged(f):
-  @wraps(f)
-  def wrapped(*args, **kwargs):
-    log.info('%s(%s)', f.__name__, ','.join([str(item) for item in args[1:]]))
-    return f(*args, **kwargs)
-  return wrapped
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        log.info('%s(%s)', f.__name__, ','.join([str(item) for item in args[1:]]))
+        return f(*args, **kwargs)
+    return wrapped
 
 
 
 class Passthrough(Operations):
-  """A simple passthrough interface.
+    """A simple passthrough interface.
 
-  This class passes all file system operations through to the operating
-  system.
+    This class passes all file system operations through to the operating
+    system.
 
-  """
-  def __init__(self, root):
-    self.root = root
-
-  def _full_path(self, partial):
-    """Calculate full path for the mounted file system.
-
-      .. note::
-
-        This isn't the same as the full path for the underlying file system.
-        As such, you can't use os.path.abspath to calculate this, as that
-        won't be relative to the mount point root.
+    Initialize the filesystem. This function can often be left unimplemented, but
+    it can be a handy way to perform one-time setup such as allocating
+    variable-sized data structures or initializing a new filesystem. The
+    fuse_conn_info structure gives information about what features are supported
+    by FUSE, and can be used to request certain capabilities (see below for more
+    information). The return value of this function is available to all file
+    operations in the private_data field of fuse_context. It is also passed as a
+    parameter to the destroy() method.
 
     """
-    if partial.startswith("/"):
-      partial = partial[1:]
-    path = os.path.join(self.root, partial)
-    return path
+    def __init__(self, root):
+        self.root = root
 
-  @logged
-  def access(self, path, mode):
-    full_path = self._full_path(path)
-    if not os.access(full_path, mode):
-      raise FuseOSError(errno.EACCES)
+    def _full_path(self, partial):
+        """Calculate full path for the mounted file system.
 
-  @logged
-  def chmod(self, path, mode):
-    full_path = self._full_path(path)
-    return os.chmod(full_path, mode)
+          .. note::
 
-  @logged
-  def chown(self, path, uid, gid):
-    full_path = self._full_path(path)
-    return os.chown(full_path, uid, gid)
+            This isn't the same as the full path for the underlying file system.
+            As such, you can't use os.path.abspath to calculate this, as that
+            won't be relative to the mount point root.
 
-  @logged
-  def getattr(self, path, fh=None):
-    full_path = self._full_path(path)
-    st = os.lstat(full_path)
-    return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-           'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+        """
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
 
-  @logged
-  def readdir(self, path, fh):
-    full_path = self._full_path(path)
+    @logged
+    def access(self, path, mode):
+        full_path = self._full_path(path)
+        if not os.access(full_path, mode):
+            raise FuseOSError(errno.EACCES)
 
-    dirents = ['.', '..']
-    if os.path.isdir(full_path):
-      dirents.extend(os.listdir(full_path))
-    for r in dirents:
-      yield r
+    @logged
+    def chmod(self, path, mode):
+        full_path = self._full_path(path)
+        return os.chmod(full_path, mode)
 
-  @logged
-  def readlink(self, path):
-    pathname = os.readlink(self._full_path(path))
-    if pathname.startswith("/"):
-      # Path name is absolute, sanitize it.
-      return os.path.relpath(pathname, self.root)
-    else:
-      return pathname
+    @logged
+    def chown(self, path, uid, gid):
+        full_path = self._full_path(path)
+        return os.chown(full_path, uid, gid)
 
-  @logged
-  def mknod(self, path, mode, dev):
-    return os.mknod(self._full_path(path), mode, dev)
+    @logged
+    def getattr(self, path, fh=None):
+        full_path = self._full_path(path)
+        st = os.lstat(full_path)
+        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+               'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
-  @logged
-  def rmdir(self, path):
-    full_path = self._full_path(path)
-    return os.rmdir(full_path)
+    @logged
+    def readdir(self, path, fh):
+        full_path = self._full_path(path)
 
-  @logged
-  def mkdir(self, path, mode):
-    return os.mkdir(self._full_path(path), mode)
+        dirents = ['.', '..']
+        if os.path.isdir(full_path):
+            dirents.extend(os.listdir(full_path))
+        for r in dirents:
+            yield r
 
-  @logged
-  def statfs(self, path):
-    full_path = self._full_path(path)
-    stv = os.statvfs(full_path)
-    return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-      'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-      'f_frsize', 'f_namemax'))
+    @logged
+    def readlink(self, path):
+        pathname = os.readlink(self._full_path(path))
+        if pathname.startswith("/"):
+            # Path name is absolute, sanitize it.
+            return os.path.relpath(pathname, self.root)
+        else:
+            return pathname
 
-  @logged
-  def unlink(self, path):
-    return os.unlink(self._full_path(path))
+    @logged
+    def mknod(self, path, mode, dev):
+        return os.mknod(self._full_path(path), mode, dev)
 
-  @logged
-  def symlink(self, target, name):
-    return os.symlink(self._full_path(target), self._full_path(name))
+    @logged
+    def rmdir(self, path):
+        full_path = self._full_path(path)
+        return os.rmdir(full_path)
 
-  @logged
-  def rename(self, old, new):
-    return os.rename(self._full_path(old), self._full_path(new))
+    @logged
+    def mkdir(self, path, mode):
+        return os.mkdir(self._full_path(path), mode)
 
-  @logged
-  def link(self, target, name):
-    return os.link(self._full_path(target), self._full_path(name))
+    @logged
+    def statfs(self, path):
+        full_path = self._full_path(path)
+        stv = os.statvfs(full_path)
+        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
+          'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
+          'f_frsize', 'f_namemax'))
 
-  @logged
-  def utimens(self, path, times=None):
-    return os.utime(self._full_path(path), times)
+    @logged
+    def unlink(self, path):
+        return os.unlink(self._full_path(path))
 
-  @logged
-  def open(self, path, flags):
-    full_path = self._full_path(path)
-    return os.open(full_path, flags)
+    @logged
+    def symlink(self, target, name):
+        return os.symlink(self._full_path(target), self._full_path(name))
 
-  @logged
-  def create(self, path, mode, fi=None):
-    full_path = self._full_path(path)
-    return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+    @logged
+    def rename(self, old, new):
+        return os.rename(self._full_path(old), self._full_path(new))
 
-  @logged
-  def read(self, path, length, offset, fh):
-    os.lseek(fh, offset, os.SEEK_SET)
-    return os.read(fh, length)
+    @logged
+    def link(self, target, name):
+        return os.link(self._full_path(target), self._full_path(name))
 
-  @logged
-  def write(self, path, buf, offset, fh):
-    os.lseek(fh, offset, os.SEEK_SET)
-    return os.write(fh, buf)
+    @logged
+    def utimens(self, path, times=None):
+        return os.utime(self._full_path(path), times)
 
-  @logged
-  def truncate(self, path, length, fh=None):
-    full_path = self._full_path(path)
-    with open(full_path, 'r+') as f:
-      f.truncate(length)
+    @logged
+    def open(self, path, flags):
+        full_path = self._full_path(path)
+        return os.open(full_path, flags)
 
-  @logged
-  def flush(self, path, fh):
-    return os.fsync(fh)
+    @logged
+    def create(self, path, mode, fi=None):
+        full_path = self._full_path(path)
+        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
 
-  @logged
-  def release(self, path, fh):
-    return os.close(fh)
+    @logged
+    def read(self, path, length, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.read(fh, length)
 
-  @logged
-  def fsync(self, path, fdatasync, fh):
-     return self.flush(path, fh)
+    @logged
+    def write(self, path, buf, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.write(fh, buf)
+
+    @logged
+    def truncate(self, path, length, fh=None):
+        full_path = self._full_path(path)
+        with open(full_path, 'r+') as f:
+            f.truncate(length)
+
+    @logged
+    def flush(self, path, fh):
+        return os.fsync(fh)
+
+    @logged
+    def release(self, path, fh):
+        return os.close(fh)
+
+    @logged
+    def fsync(self, path, fdatasync, fh):
+        return self.flush(path, fh)
